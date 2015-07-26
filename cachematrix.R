936dc58a122f192f01dcd74c03d0199510c5ed7b
= <=>
2015-07-26 13:38:25 -0700

    Change for assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+## This function is used to get list which is used in calculating inverse of matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  invrs = NULL
+  set = function(y) {
+    # use `<<-` to assign a value to an object in an environment 
+    # different from the current environment. 
+    x <<- y
+    inv <<- NULL
+  }
+  get = function() x
+  setinv = function(inverse) invrs <<- inverse 
+  getinv = function() invrs
+  list(set=set, get=get, setinv=setinv, getinv=getinv)
 }
 
 
-## Write a short comment describing this function
+
+## calculate inverse and set in cache
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  invrs = x$getinv()
+  
+  if (!is.null(invrs)){
+    message("getting cached data")
+    return(invrs)
+  }
+  
+
+  mat.data = x$get()
+  inv = solve(mat.data, ...)
+  
+  x$setinv(invrs)
+  
+  return(invrs)
 }
