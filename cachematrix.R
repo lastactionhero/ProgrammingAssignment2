d7a4ae3dff71dacd5a80afb28a8d92d26ee4a59d
= <=>
2016-01-21 10:31:34 -0800

    Comments added to Solution of assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,10 @@
 
 ## This function is used to get list which is used in calculating inverse of matrix.
+# It returns following list of functions
+# set := set matrix data as per argument and null to inverse of matrix
+# get := get the matrix data
+# setinv := set inverse as per argument
+# getinv := return inverse
 
 makeCacheMatrix <- function(x = matrix()) {
   
@@ -19,19 +24,19 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## calculate inverse and set in cache
-
+# This function returns inverse of matrix if its already calculated otherwise calculates inverse and store it.
 cacheSolve <- function(x, ...) {
   
   invrs = x$getinv()
-  
+  # check if inverse already calculated if yes then return it.
   if (!is.null(invrs)){
     message("getting cached data")
     return(invrs)
   }
   
-
-  mat.data = x$get()
-  invrs = solve(mat.data, ...)
+#calculate, store and return inverse.
+  data = x$get()
+  invrs = solve(data, ...)
   
   x$setinv(invrs)
   
